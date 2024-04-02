39848405b5fe7dd55932956066c60811cadb2900
Deepak Sankaran <sdeepak101@gmail.com>
2024-04-02 12:33:14 +0530

    changes

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -17,6 +17,7 @@ public class DevopsIntegrationApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
